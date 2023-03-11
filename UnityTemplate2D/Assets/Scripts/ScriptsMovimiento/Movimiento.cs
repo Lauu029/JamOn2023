9afed94b2fc476b01eb656e2f3f2d8c6c543cd03
sergimol <sergimol@ucm.es>
2023-03-11 02:24:14 +0100

    Nano mesa

---
+++ UnityTemplate2D/Assets/Scripts/ScriptsMovimiento/Movimiento.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class Movimiento : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private PlayerController playerController;
+
+    private float velocity;
+
+    [SerializeField]
+    float initialVelocity;
+
+    [SerializeField]
+    float maxVelocity;
+
+    [SerializeField]
+    float velocityFactor;
+
+    [SerializeField]
+    float deceleration;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerController = new PlayerController();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 moveVal = playerController.Player.Move.ReadValue<Vector2>();
+        if (moveVal.x != 0)
+        {
+            velocity = Mathf.Min(velocity + velocityFactor * Time.deltaTime, maxVelocity);
+            Vector2 input = new Vector2(moveVal.x, rb.velocity.y).normalized * velocity;
+
+            rb.velocity = input;
+        }
+        else if(rb.velocity.x != 0)
+        {
+            float sign = rb.velocity.x / Mathf.Abs(rb.velocity.x);
+            float newVel = Mathf.Max(Mathf.Abs(rb.velocity.x) - deceleration * Time.deltaTime, 0);
+            rb.velocity = new Vector2(newVel * sign, rb.velocity.y);
+            Debug.Log(rb.velocity);
+            velocity = /*Mathf.Max(velocity - velocityFactor * Time.deltaTime,*/ initialVelocity;
+        }
+    }
+
+    private void OnEnable()
+    {
+        playerController.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerController.Player.Disable();
+    }
+}
